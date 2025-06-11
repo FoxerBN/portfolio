907b2dfc9075dc50629a439abaf481e37ea07902
foxerBN <barspin4499@gmail.com>
2025-06-11 19:40:50 +0200

    portfolio download link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,6 +7,11 @@ export default function Footer() {
   return (
     <footer className="w-full py-4 px-6 text-center text-sm mt-auto">
       © {new Date().getFullYear()} {t('footer')}
+      <span>
+        
+        <a href="/Richard-Tekula-CV.pdf" download target="_blank" rel="noopener noreferrer">{"->Download CV-<"}
+        </a>
+      </span>
     </footer>
   );
 }
